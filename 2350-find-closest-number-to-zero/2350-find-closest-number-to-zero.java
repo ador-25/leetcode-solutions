039db6b25c4d56915648662b05c127e2950f52b8
Abtahee Farhan Ador <84260545+ador-25@users.noreply.github.com>
2025-01-11 23:10:23 +0600

    2350-FIND-CLOSEST-NUMBER-TO-ZERO Stats: Time: 2 ms (91.93%), Space: 45.3 MB (12.83%) - LeetHub

--- 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.java
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.java
@@ -1,17 +1,39 @@
 class Solution {
-    public int findClosestNumber(int[] nums) {
-        int min = Math.abs(nums[0]);
-        int result = nums[0];
-        for (int i=1;i<nums.length;i++){
-            if (Math.abs(min)>=Math.abs(nums[i])){
-                min = nums[i];
-                if (Math.abs(min)== Math.abs(result)){
-                    result =Math.max(result,min);
-                    continue;
-                }
-                result = min;
+       public int findClosestNumber(int[] nums) {
+        int positiveMin = Integer.MAX_VALUE;
+
+        int negativeMax = Integer.MIN_VALUE;
+        int negativeCount = 0,positiveCount = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                return 0;
+            }
+            // calculate positive min
+            if (nums[i]>0){
+                positiveMin = Math.min(positiveMin,nums[i]);
+                positiveCount++;
+            }
+            else{  // calculate negative max
+                negativeMax = Math.max(negativeMax,nums[i]);
+                negativeCount++;
             }
         }
-        return result;
+
+
+        if (negativeCount == 0){
+            return positiveMin;
+        }
+        if (positiveCount == 0){
+            return negativeMax;
+        }
+        if (positiveMin != negativeMax){
+            if (Math.abs(negativeMax)<positiveMin){
+                return negativeMax;
+            }
+            return positiveMin;
+        }
+        return positiveMin;
+
     }
 }
