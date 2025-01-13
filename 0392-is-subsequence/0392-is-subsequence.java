9fe6cfd04904da11a45546a3c1f4f9f3b944eff2
Abtahee Farhan Ador <84260545+ador-25@users.noreply.github.com>
2025-01-13 15:27:41 +0600

    0392-IS-SUBSEQUENCE Stats: Time: 2 ms (65.8%), Space: 41.8 MB (21.89%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,21 +1,12 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if (s.length() > t.length()){
-            return false;
-        }
-        if (s.length()==0){
-            return true;
-        }
-        int slowRunner =0;
-        for(int i=0;i<t.length();i++){
-            char ch = t.charAt(i);
-            if (ch == s.charAt(slowRunner)){
-                slowRunner++;
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
             }
-            if (slowRunner == s.length()){
-                return true;
-            }
+            j++;
         }
-        return false;
+        return i == s.length();
     }
 }
