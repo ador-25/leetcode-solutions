d878656e6cb27b1d1c2c190771cb498425b0af1e
Abtahee Farhan Ador <84260545+ador-25@users.noreply.github.com>
2025-01-23 14:21:14 +0600

    0242-VALID-ANAGRAM Stats: Time: 3 ms (90.19%), Space: 42.9 MB (89.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,14 +4,11 @@ class Solution {
             return false;
         }
         char[] m1 = new char[256];
-        for (int i=0;i<s.length();i++){
-            m1[s.charAt(i)]++;
-        }
+        for (int i=0;i<s.length();i++) m1[s.charAt(i)]++;
 
         for(int i=0;i<t.length();i++){
-            if (m1[t.charAt(i)]==0){
+            if (m1[t.charAt(i)]==0)
                 return false;
-            }
             m1[t.charAt(i)]--;
         }
 
