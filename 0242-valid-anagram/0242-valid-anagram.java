e974c6c79cdb6cb1f902260c770da3e34ac80dae
Abtahee Farhan Ador <84260545+ador-25@users.noreply.github.com>
2025-08-10 02:03:59 +0600

    Time: 3 ms (92%), Space: 43 MB (88.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()){
+        if(s.length()!=t.length())
             return false;
-        }
-        char[] m1 = new char[256];
-        for (int i=0;i<s.length();i++) m1[s.charAt(i)]++;
+        int[]sourceMap =  new int[256];
+        int[]destMap =  new int[256];
 
-        for(int i=0;i<t.length();i++){
-            if (m1[t.charAt(i)]==0)
+        for( int i=0;i<s.length();i++){
+            sourceMap[s.charAt(i)]++;
+        }
+        for( int i=0;i<t.length();i++){
+            destMap[t.charAt(i)]++;
+        }
+
+        for(int i=97;i<=122;i++){
+            if(sourceMap[i]!=destMap[i]){
                 return false;
-            m1[t.charAt(i)]--;
+            }
         }
-
         return true;
+
     }
-
-
 }
