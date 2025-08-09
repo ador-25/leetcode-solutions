77dc7366183e4d5c4591b8b6332a669fe1652bf6
Abtahee Farhan Ador <84260545+ador-25@users.noreply.github.com>
2025-08-10 01:47:23 +0600

    Time: 1 ms (99.7%), Space: 42.4 MB (16.46%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.java
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -1,11 +1,10 @@
 class Solution {
     public int scoreOfString(String s) {
-        int sum =0;
+        int sum = 0;
         int prev = s.charAt(0);
-
-        for (int i =1;i<s.length();i++){
+        for (int i = 1; i < s.length(); i++) {
             int curr = s.charAt(i);
-            int abs = Math.abs(curr-prev);
+            int abs = Math.abs(curr - prev);
             sum += abs;
             prev = s.charAt(i);
         }
