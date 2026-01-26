96705f1cba8b0f4a6eec827ab1a0479299bdb8ef
Abtahee Farhan Ador <84260545+ador-25@users.noreply.github.com>
2026-01-26 12:20:07 +0600

    0283-MOVE-ZEROES Stats: Time: 2 ms (89.89%), Space: 47.9 MB (28.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int front =0;
+
+        int f =0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
                 int temp=nums[i];
-                nums[i]=nums[front];
-                nums[front]=temp;
-                front ++;
+                nums[i]=nums[f];
+                nums[f]=temp;
+                f ++;
             }
         }
         
