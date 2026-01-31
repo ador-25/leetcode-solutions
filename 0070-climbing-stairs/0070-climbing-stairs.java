afb1264d464d74e9caed541679549af880a74273
Abtahee Farhan Ador <84260545+ador-25@users.noreply.github.com>
2026-01-31 21:56:53 +0600

    0070-CLIMBING-STAIRS Stats: Time: 55 ms (0.38%), Space: 42.4 MB (12.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,12 @@
 class Solution { 
-    private int[]cache;
     public int climbStairs(int n) {
-        cache = new int[n+1];
-        return climb(n);
-    }
-
-    private int climb(int n){
-        if(n==1) return 1;
-        if(n==2) return 2;
-        if (cache[n]!=0) return cache[n];
-        int l = climb(n-1);
-        int r = climb(n-2);
-        int result = l+r;
-        cache[n]=result;
-        return result;
+        if(n==1 || n==2) return n;
+        int[]cache = new int[n+1];
+        cache[1]=1;
+        cache[2]=2;
+        for(int i=3;i<=n;i++){
+            cache[i]=cache[i-1]+cache[i-2];
+        }
+        return cache[n];
     }
 }
